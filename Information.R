1f82284bcb56db5ffbc29cbeb1c3598f9a0939ab
Waseem Hussain <waseemhussain907@gmail.com>
2018-07-02 16:29:13 -0500

    codes updated

--- Information.R
+++ Information.R
@@ -60,13 +60,14 @@ column(8,
         ), style = "font-size: 16px; line-height: 1.7;"),
     hr(),
     
+    h2(strong('Demo how to use the application cab be found on youtube at:'),
+       tags$a(href="https://youtu.be/5-JLMpSiwv4", "Demo on Youtube"),
+       style = "font-size: 18px"),
+    hr(),
     
     h2(strong('Shiny Application is Hosted at:'),
      tags$a(href="https://chikudaisei.shinyapps.io/shinyaim/", "ShinyAIM"),
-       style = "font-size: 16px"),
-     tags$div(
-      tags$a(href="https://github.com/whussain2/ShinyAIM", "ShinyAIM:Website"),
-      style = "font-size: 18px;"),
+       style = "font-size: 18px"),
     hr(),
       
       h2(strong('Source Code and Sample Files')),
@@ -90,7 +91,7 @@ column(8,
       tags$div(
         tags$p('We thank Yan Holtz for the useful R code provided on the webpage (link given below), some of the codes has been adopted in this work to plot the combined Manhattan plot'), 
         tags$a(href="https://www.r-graph-gallery.com/wp-content/uploads/2018/02/Manhattan_plot_in_R.html", "Webpage available"),
-        style = "font-size: 16px"),
+        style = "font-size: 18px"),
         hr(),
       tags$div(
         tags$footer('Copyright (c) 2018, Waseem Hussain,  code licensed under Artistic License 2.0.'),
